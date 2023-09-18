4492a126fb29814c7d525e038a07a4527616677f
mahamudulhasan <mahamudulhasaan5@gmail.com>
2023-09-18 16:11:48 +0600

    add option doen

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 let input = prompt("what would you like do?");
+const toDos = ["brush my teeth", "eat my brakfast"];
 while (input !== "quit") {
-  let input = prompt("what would you like do?");
+  if (input === "list") {
+    console.log("***************");
+    for (i = 0; i < toDos.length; i++) {
+      console.log(`${i}:${toDos[i]}`);
+    }
+    console.log("***************");
+  } else if (input === "new") {
+    const newTodo = prompt("what you want to add");
+    toDos.push(newTodo);
+  } else if (input === "delete") input = prompt("what would you like do?");
 }
 console.log("ok,quit the app");
